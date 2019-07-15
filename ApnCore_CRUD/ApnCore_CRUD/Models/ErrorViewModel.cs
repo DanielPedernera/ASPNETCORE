2edb7e4f94b8c912c4cc74e9919a6c62e91f7d45
Daniel Mauricio Pedernera Parada <daniel7pp@hotmail.es>
2019-07-15 00:10:40 -0300

    Comit Inicial

---
+++ ApnCore_CRUD/ApnCore_CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApnCore_CRUD.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
